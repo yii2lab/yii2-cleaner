02697a1b0d82bdd798c1b55629e31b39a50d8d8f
Vitaliy <theyamshikov@yandex.ru>
2017-06-22 23:32:48 +0600

    init files

---
+++ src/Module.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace yii2lab\cleaner;
+
+use yii\base\Module as YiiModule;
+
+/**
+ * cleaner module definition class
+ */
+class Module extends YiiModule
+{
+	
+}
---
+++ src/controllers/DefaultController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace yii2lab\cleaner\controllers;
+
+use woop\extension\console\yii\console\Controller;
+use yii2lab\cleaner\helpers\ClearHelper;
+use woop\extension\console\helpers\Output;
+use woop\extension\console\helpers\input\Select;
+
+class DefaultController extends Controller
+{
+	
+	public function actionIndex($option = null)
+	{
+		$allNames = [
+			'web/assets',
+			'runtime',
+			'runtime/cache',
+			'tests/_output',
+		];
+		$answer = Select::display('Select objects', $allNames, 1);
+		$result = ClearHelper::run($answer);
+		if($result) {
+			Output::items($result, "Clear completed: " . count($result) . " objects");
+		} else {
+			Output::block("Not fount object for clear!");
+		}
+	}
+	
+}
