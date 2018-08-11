23ce5622205f20e3c1c8608c69b8b0cef53ec5d6
Vitaliy <theyamshikov@yandex.ru>
2018-08-11 22:20:20 +0600

    upgrade rbac

--- src/admin/Module.php
+++ src/admin/Module.php
@@ -3,6 +3,9 @@
 namespace yii2module\cleaner\admin;
 
 use yii\base\Module as YiiModule;
+use yii2lab\helpers\Behavior;
+use yii2module\cleaner\domain\enums\CleanerPermissionEnum;
+
 
 /**
  * app module definition class
@@ -10,4 +13,11 @@ use yii\base\Module as YiiModule;
 class Module extends YiiModule
 {
 	public static $langDir = '@yii2module/cleaner/domain/messages';
+
+    public function behaviors()
+    {
+        return [
+            'access' => Behavior::access(CleanerPermissionEnum::MANAGE),
+        ];
+    }
 }
