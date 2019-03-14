ea6e915d61abca7996a3a524e8e360dd69dd90c2
amubarak <amubarak@wooppay.com>
2019-03-14 16:50:00 +0600

    me

---
+++ src/domain/messages/uz/cache.php
@@ -0,0 +1,12 @@
+﻿<?php
+return [
+	'title'=>'Кэшни бошқариш',
+	'cache'=>'Кэш',
+	'common_app'=>'Умумий кэшни тозалаш',
+	'backend_app'=>'Админка кэшини тозалаш',
+	'frontend_app'=>'Фронтенд кэшини тозалаш',
+	'api_app'=>'Rest API кэшини тозалаш',
+	
+	'select_cache_section_to_flush'=>'Кэшни қаерда тозалашни кўрсатинг',
+	'cache_successfully_flushed'=>'Кэш муваффақиятли тозаланди',
+];
